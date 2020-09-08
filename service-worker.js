e59bd6fcfac0cd9fe803d66f00d711c2ec3cc924
Oleg <mvp.westbrook4k@gmail.com>
2020-09-08 07:19:49 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.52b33927b054831acc017c3a16431f51.js"
 );
 
 self.addEventListener('message', (event) => {
